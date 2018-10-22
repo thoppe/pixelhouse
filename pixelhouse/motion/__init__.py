887bd30fc11b9bf360c38daa10220f66c7845546
Travis Hoppe <travis.hoppe@gmail.com>
2018-10-22 16:38:42 -0400

    Made easing into a proper motion library

---
+++ pixelhouse/motion/__init__.py
@@ -0,0 +1,10 @@
+from .easing import easeInSine, easeOutSine, easeInOutSine
+from .easing import easeInQuad, easeOutQuad, easeInOutQuad
+from .easing import easeInCubic, easeOutCubic, easeInOutCubic
+from .easing import easeInQuart, easeOutQuart, easeInOutQuart
+from .easing import easeInQuint, easeOutQuint, easeInOutQuint
+from .easing import easeInExpo, easeOutExpo, easeInOutExpo
+from .easing import easeInCirc, easeOutCirc, easeInOutCirc
+from .easing import easeInBack, easeOutBack, easeInOutBack
+
+from .easing import offsetEase, easeReturn
