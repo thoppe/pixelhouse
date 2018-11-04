664aaf9a29d99408b147a53eedf754afb2c7265a
Travis Hoppe <travis.hoppe@gmail.com>
2018-11-04 18:32:53 -0500

    code cleanup

--- unincorporated_demos/gradient.py
+++ unincorporated_demos/gradient.py
@@ -1,19 +1,14 @@
 # A working file to test various aspects of the module
 import numpy as np
 from pixelhouse import Canvas, circle
-from pixelhouse.gradient.gradient import linear_gradient 
+from pixelhouse.gradient import linear_gradient 
 
 sx = 400
 
 c1 = Canvas(width=sx, height=sx, bg='white')
 circle(color='purple')(c1)
 
-c2 = Canvas(width=sx, height=sx, bg='white')
-
-linear_gradient(color0='black')(c2, mask=c1)
-
-#c2._img[c.mask] = [155,155,155,255]
-#print(c.img)
-#exit()
+c2 = Canvas(width=sx, height=sx, bg='purple')
+linear_gradient(color0='black', color1=[255,255,255,55])(c2, mask=c1)
 
 c2.show()
