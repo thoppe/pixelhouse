31f13508e86ef691ffb9ff14a673c8d168c30911
Travis Hoppe <travis.hoppe@gmail.com>
2018-11-04 21:36:29 -0500

    Add LAB interpolation

--- pixelhouse/gradient/gradient.py
+++ pixelhouse/gradient/gradient.py
@@ -2,11 +2,47 @@ import numpy as np
 import cv2
 from ..artist import Artist, constant
 from ..primitives import _DEFAULT_COLOR, _DEFAULT_SECONDARY_COLOR
+import cv2
 
-class LocalCoordinateArtist(Artist):
-    pass
+def lerp(t, y0, y1, x0=0, x1=1):
+    return np.interp(t, [x0, x1], [y0, y1])
 
-class linear_gradient(LocalCoordinateArtist):
+def RGBa_interpolation(t, c0, c1, alpha):
+    R = lerp(t, c0[0], c1[0])
+    G = lerp(t, c0[1], c1[1])
+    B = lerp(t, c0[2], c1[2])
+    a = lerp(t, c0[3], c1[3])*alpha
+    return np.vstack([R,G,B,a]).T
+
+def LABa_interpolation(t, c0, c1, alpha):
+    # Slice off the alpha channel
+    a_start, c0 = c0[3], c0[:3]
+    a_final, c1 = c1[3], c1[:3]
+
+    # Convert colors to LAB    
+    C = np.array([c0,c1]).astype(np.uint8).reshape((2,1,3))
+    c0, c1 = cv2.cvtColor(C, cv2.COLOR_RGB2LAB).reshape(2,3)
+
+    # Interpolate in LAB space
+    L = lerp(t, c0[0], c1[0])
+    A = lerp(t, c0[1], c1[1])
+    B = lerp(t, c0[2], c1[2])
+    
+    # Convert back to RGB
+    C = np.vstack([L,A,B]).T.reshape(-1,1,3).astype(np.uint8)
+    C = cv2.cvtColor(C, cv2.COLOR_LAB2RGB).reshape(-1,3)
+
+    # Add back in the weighted alpha channel
+    a = lerp(t, a_start, a_final)
+    a = np.clip(a*alpha, 0, 255).astype(np.uint8)
+
+    # Stack them all together
+    C = np.hstack([C,a.reshape(-1,1)])
+
+    return C
+    
+
+class linear_gradient(Artist):
     color0 = constant(_DEFAULT_COLOR)
     color1 = constant(_DEFAULT_COLOR)
     theta = constant(np.pi/4)
@@ -31,17 +67,15 @@ class linear_gradient(LocalCoordinateArtist):
         c0 = cvs.transform_color(self.color0(t))
         c1 = cvs.transform_color(self.color1(t))
 
-        # RGBA interpolation (not great!)
-        R = np.interp(pro, [0,1], [c0[0], c1[0]])
-        G = np.interp(pro, [0,1], [c0[1], c1[1]])
-        B = np.interp(pro, [0,1], [c0[2], c1[2]])
-        A = np.interp(pro, [0,1], [c0[3], c1[3]])
-
         # Smooth the image based off the alpha from the mask image
         alpha = (mask.alpha/255.0)[mask_idx]
-        A *= alpha
+
+        # RGBA interpolation (not great!)
+        #C = RGBa_interpolation(pro, c0, c1,alpha)
+
+        # LAB interpolation (better!)
+        C = LABa_interpolation(pro, c0, c1,alpha)
         
-        C = np.clip(np.vstack([R,G,B,A]).T, 0, 255).astype(np.uint8)
 
         rhs = cvs.copy()
         rhs._img[mask_idx] = C
--- unincorporated_demos/blends.py
+++ unincorporated_demos/blends.py
@@ -5,10 +5,16 @@ from pixelhouse.gradient import linear_gradient
 
 sx = 400
 
+
+c0 = Canvas(width=sx, height=sx, bg='white')
+circle(x=-3,r=0.25)(c0)
+
 c1 = Canvas(width=sx, height=sx, bg='white')
-circle(color='purple')(c1)
+circle()(c1)
+
 
 c2 = Canvas(width=sx, height=sx, bg='purple')
-linear_gradient(color0='black', color1=[255,255,255,55])(c2, mask=c1)
+linear_gradient(color0='white', color1=[255,255,155,0])(c2, mask=c1)
+linear_gradient(color0='red', color1='green')(c2, mask=c0)
 
 c2.show()
