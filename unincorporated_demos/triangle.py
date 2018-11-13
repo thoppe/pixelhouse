9dd0ed3b391740452c20057bd271ea73318349b7
Travis Hoppe <travis.hoppe@gmail.com>
2018-11-12 22:05:30 -0500

    Fix bug by filling in missing value

--- unincorporated_demos/triangle.py
+++ unincorporated_demos/triangle.py
@@ -1,14 +1,22 @@
 # A working file to test various aspects of the module
 import numpy as np
+import pixelhouse as ph
 from pixelhouse import Canvas, polyline, circle
+from pixelhouse.transform import rotate, translate
 
-c1 = Canvas(width=400, height=400, bg="white")
-c1(polyline(color="k"))
+pal = ph.ColorLoversPalette()(2)
 
-c2 = Canvas(width=400, height=400, bg="white")
-c2(circle(color="teal"))
+
+c1 = Canvas(width=400, height=400, bg=pal[0])
+c1(polyline(color='k'))
+
+c2 = Canvas(width=400, height=400, bg=pal[0])
+c2(circle(color=pal[2]))
 
 C = c1.blank()
 C += c2
 C += c1
+
+C += rotate(np.pi/4)
+C += translate(1.0)
 C.show()
