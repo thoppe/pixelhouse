0fb6542d8729b162812aae07a01e0a17b85c93b3
Travis Hoppe <travis.hoppe@gmail.com>
2018-11-15 14:05:26 -0500

    Fixed bg bug with named colors

--- pixelhouse/transform/simple.py
+++ pixelhouse/transform/simple.py
@@ -25,9 +25,10 @@ class translate(Artist):
     def draw(self, cvs, t=0.0):
         x = cvs.transform_length(self.x(t))
         y = cvs.transform_length(self.y(t))
+        bg = cvs.transform_color(cvs.bg)
 
         M = np.float32([[1, 0, x], [0, 1, y]])
-        cvs._img = cv2.warpAffine(cvs.img, M, cvs.shape[:2], borderValue=cvs.bg)
+        cvs._img = cv2.warpAffine(cvs.img, M, cvs.shape[:2], borderValue=bg)
 
 
 class rotate(Artist):
@@ -40,9 +41,8 @@ class rotate(Artist):
         theta = cvs.transform_angle(self.theta(t))
         x = cvs.transform_x(self.x(t))
         y = cvs.transform_y(self.y(t))
-
-        print(x, y)
+        bg = cvs.transform_color(cvs.bg)
 
         cols, rows = cvs.shape[:2]
         M = cv2.getRotationMatrix2D((x, y), theta, 1)
-        cvs._img = cv2.warpAffine(cvs.img, M, (cols, rows), borderValue=cvs.bg)
+        cvs._img = cv2.warpAffine(cvs.img, M, (cols, rows), borderValue=bg)
--- unincorporated_demos/playground5.py
+++ unincorporated_demos/playground5.py
@@ -16,15 +16,14 @@ lg = ph.linear_gradient(pal[0], pal[1])
 
 # for i in np.arange(-6, 6, 1.0):
 #    C += ph.text("pixelhouse", font=f_font, y=i, gradient=lg)
-C += ph.text("pixelhouse", font=f_font)
+C += ph.text("pixelhouse", font=f_font,color='w')
 
 # C += el.pull(alpha=0.05)
 # C += el.distort(alpha=20)
-z = np.logspace(-0.8, -1.5, 100)
-print(z)
-C += el.distort(sigma=z, seed=20)
-
-z2 = 0.15 - z
+#z = np.logspace(-0.8, -1.5, 100)
+#print(z)
+#C += el.distort(sigma=z, seed=20)
+#z2 = 0.15 - z
 # C += gaussian_blur(z2,z2)
 # C.save("logo_pixelhouse.png")
 C.show()
