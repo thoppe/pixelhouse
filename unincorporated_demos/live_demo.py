0c2633f123af34aefb0e6ccbd7de1e225c7bccc8
Travis Hoppe <travis.hoppe@gmail.com>
2018-11-15 00:03:15 -0500

    Add H&&T demo

---
+++ unincorporated_demos/live_demo.py
@@ -0,0 +1,35 @@
+from pixelhouse import *
+from pixelhouse.filter import *
+from pixelhouse.motion import *
+from pixelhouse.transform import *
+import numpy as np
+
+pal = ColorLoversPalette()(15)
+
+C = Canvas(400, 400, bg=pal[0])
+C = Animation(400, 400, bg=pal[0])
+
+y = motion.easeReturn("easeInOutQuad", 2, -2, len(C))
+
+def draw_circles(C):
+    C += circle(-2, y, r=0.20, color=pal[1])
+    C += circle(-1, -y, r=0.20, color=pal[2])
+    C += circle( 1, y, r=0.20, color=pal[1])
+    C += circle( 2, -y, r=0.20, color=pal[2])
+
+
+draw_circles(C)
+C += gaussian_blur(.5,.5)
+draw_circles(C)
+
+with C.layer() as L:
+    theta = easeInOutQuad(0, 2*np.pi,len(C))()
+    L += rectangle(-.5, -.5, .5, .5, color=pal[3])
+    L += rotate(theta)
+    
+f_font = "../pixelhouse/fonts/Montserrat-Medium.otf"
+g = linear_gradient(pal[2], pal[3])
+C += text("H&&T presents", y=3, font_size=0.5, font=f_font, gradient=g)
+C += text("pixelhouse", y=-3, font_size=0.5, font=f_font, gradient=g)
+
+C.show()
