71c96957aca306710fceb4eea72a6e68ffe97f3e
Travis Hoppe <travis.hoppe@gmail.com>
2018-12-02 00:48:38 -0500

    Started refactoring easing as a function

--- pixelhouse/motion/bezier.py
+++ pixelhouse/motion/bezier.py
@@ -20,11 +20,13 @@ class bezierMotionCurve:
     Bezier curve with endpoints set at (0,0) and (1,1)
     """
 
-    def __init__(self, x1, y1, x2, y2):
+    def __init__(self, x1, y1, x2, y2, yvals_only=False):
         points = np.array([(0, 0), (x1, y1), (x2, y2), (1, 1)])
         self.xPoints = np.array([p[0] for p in points])
         self.yPoints = np.array([p[1] for p in points])
         self.nPoints = 4
+
+        self.yvals_only = yvals_only
 
     def __call__(self, t):
         if not isinstance(t, collections.Iterable):
@@ -40,4 +42,7 @@ class bezierMotionCurve:
         xvals = np.dot(self.xPoints, polynomial_array)
         yvals = np.dot(self.yPoints, polynomial_array)
 
+        if self.yvals_only:
+            return yvals
+        
         return xvals, yvals
--- pixelhouse/motion/easing.py
+++ pixelhouse/motion/easing.py
@@ -6,17 +6,22 @@ import math
 import numpy as np
 from .bezier import bezierMotionCurve
 
-
 class EasingBase:
-    def __init__(self, start=0, stop=1, duration=1.0):
+    def __init__(self, start=0, stop=1): #, duration=1.0):
         self.start = start
         self.stop = stop
-        self.duration = duration
+        #self.duration = duration
 
-    def __call__(self):
-        t = np.linspace(0, 1, self.duration)
-        _, a = self.func(t)
-        return self.stop * (1 - a) + self.start * a
+    def __call__(self, t):
+        a = self.func(t)
+        result = self.stop * (1 - a) + self.start * a
+
+        # If the input function call is an array, return one
+        if hasattr(t, '__iter__'):
+            return np.array(result)
+
+        # Else, return a single value
+        return result[0]
 
     @classmethod
     def func(cls, t):
@@ -25,53 +30,58 @@ class EasingBase:
 
 class BezierEase(EasingBase):
     def __init__(
-        self, x0=0.45, y0=0.25, x1=0.55, y1=0.75, start=0, stop=1, duration=1
+        self, x0=0.45, y0=0.25, x1=0.55, y1=0.75, start=0, stop=1,
     ):
+        super().__init__(start, stop)
+        self.x0 = x0
+        self.y0 = y0
+        self.x1 = x1
+        self.y1 = y1
 
-        super().__init__(start, stop, duration)
-        self.f = bezierMotionCurve(x0, y0, x1, y1)
+        # Lazy loading of the Bezier curve so we can quickly create objects
+        self.f = None
+
+    def get_params(self):
+        '''
+        Return a set of arguments to construct the easing.
+        '''
+        return (self.x0, self.y0, self.x1, self.y1)
+        
+    def _get_bezier_function(self):
+        if self.f is None:
+            self.f = bezierMotionCurve(
+                self.x0, self.y0, self.x1, self.y1, yvals_only=True)
+        return self.f
 
     def func(self, t):
-        return self.f(t)
+        return self._get_bezier_function()(t)
+    
 
+class easeInSine(BezierEase): x0, y0, x1, y1 = 0.47, 0, 0.745, 0.715
+class easeOutSine(BezierEase): x0, y0, x1, y1 = 0.39, 0.575, 0.565, 1
+class easeInOutSine(BezierEase): x0, y0, x1, y1 = 0.445, 0.05, 0.55, 0.95
+class easeInQuad(BezierEase): x0, y0, x1, y1 = 0.55, 0.085, 0.68, 0.53
+class easeOutQuad(BezierEase): x0, y0, x1, y1 = 0.25, 0.46, 0.45, 0.94
+class easeInOutQuad(BezierEase): x0, y0, x1, y1 = 0.455, 0.03, 0.515, 0.955
+class easeInCubic(BezierEase): x0, y0, x1, y1 = 0.55, 0.055, 0.675, 0.19
+class easeOutCubic(BezierEase): x0, y0, x1, y1 = 0.215, 0.61, 0.355, 1
+class easeInOutCubic(BezierEase): x0, y0, x1, y1 = 0.645, 0.045, 0.355, 1
+class easeInQuart(BezierEase): x0, y0, x1, y1 = 0.895, 0.03, 0.685, 0.22
+class easeOutQuart(BezierEase): x0, y0, x1, y1 = 0.165, 0.84, 0.44, 1
+class easeInOutQuart(BezierEase): x0, y0, x1, y1 = 0.77, 0, 0.175, 1
+class easeInQuint(BezierEase): x0, y0, x1, y1 = 0.755, 0.05, 0.855, 0.06
+class easeOutQuint(BezierEase): x0, y0, x1, y1 = 0.23, 1, 0.32, 1
+class easeInOutQuint(BezierEase): x0, y0, x1, y1 = 0.86, 0, 0.07, 1
+class easeInExpo(BezierEase): x0, y0, x1, y1 = 0.95, 0.05, 0.795, 0.035
+class easeOutExpo(BezierEase): x0, y0, x1, y1 = 0.19, 1, 0.22, 1
+class easeInOutExpo(BezierEase): x0, y0, x1, y1 = 1, 0, 0, 1
+class easeInCirc(BezierEase): x0, y0, x1, y1 = 0.6, 0.04, 0.98, 0.335
+class easeOutCirc(BezierEase): x0, y0, x1, y1 = 0.075, 0.82, 0.165, 1
+class easeInOutCirc(BezierEase): x0, y0, x1, y1 = 0.785, 0.135, 0.15, 0.86
+class easeInBack(BezierEase): x0, y0, x1, y1 = 0.6, -0.28, 0.735, 0.045
+class easeOutBack(BezierEase): x0, y0, x1, y1 = 0.175, 0.885, 0.32, 1.275
+class easeInOutBack(BezierEase): x0, y0, x1, y1 = 0.68, -0.55, 0.265, 1.55
 
-def BezierClassFactory(x0, y0, x1, y1):
-    class B(BezierEase):
-        def __init__(self, start=0, stop=1, duration=1):
-            super().__init__(x0, y0, x1, y1, start, stop, duration)
-
-    return B
-
-
-named_easings = {
-    "easeInSine": (0.47, 0, 0.745, 0.715),
-    "easeOutSine": (0.39, 0.575, 0.565, 1),
-    "easeInOutSine": (0.445, 0.05, 0.55, 0.95),
-    "easeInQuad": (0.55, 0.085, 0.68, 0.53),
-    "easeOutQuad": (0.25, 0.46, 0.45, 0.94),
-    "easeInOutQuad": (0.455, 0.03, 0.515, 0.955),
-    "easeInCubic": (0.55, 0.055, 0.675, 0.19),
-    "easeOutCubic": (0.215, 0.61, 0.355, 1),
-    "easeInOutCubic": (0.645, 0.045, 0.355, 1),
-    "easeInQuart": (0.895, 0.03, 0.685, 0.22),
-    "easeOutQuart": (0.165, 0.84, 0.44, 1),
-    "easeInOutQuart": (0.77, 0, 0.175, 1),
-    "easeInQuint": (0.755, 0.05, 0.855, 0.06),
-    "easeOutQuint": (0.23, 1, 0.32, 1),
-    "easeInOutQuint": (0.86, 0, 0.07, 1),
-    "easeInExpo": (0.95, 0.05, 0.795, 0.035),
-    "easeOutExpo": (0.19, 1, 0.22, 1),
-    "easeInOutExpo": (1, 0, 0, 1),
-    "easeInCirc": (0.6, 0.04, 0.98, 0.335),
-    "easeOutCirc": (0.075, 0.82, 0.165, 1),
-    "easeInOutCirc": (0.785, 0.135, 0.15, 0.86),
-    "easeInBack": (0.6, -0.28, 0.735, 0.045),
-    "easeOutBack": (0.175, 0.885, 0.32, 1.275),
-    "easeInOutBack": (0.68, -0.55, 0.265, 1.55),
-}
-
-for key, vals in named_easings.items():
-    locals()[key] = BezierClassFactory(*vals)
 
 #########################################################################
 # Custom Easing functions
@@ -80,7 +90,9 @@ for key, vals in named_easings.items():
 
 class offsetEase(BezierEase):
     def __init__(self, dx=0.0, dy=0.1, baseEase="easeInSine", *args, **kwargs):
-        x0, x1, y0, y1 = named_easings[baseEase]
+        ease = globals()[baseEase]
+        x0, y0, x1, y1 = ease.get_params()
+        
         x0 -= dx
         x1 += dx
         y0 -= dy
@@ -88,11 +100,12 @@ class offsetEase(BezierEase):
 
         super().__init__(x0, x1, y0, y1, *args, **kwargs)
 
-
 def easeReturn(easing_func, start, stop, frames):
     """
     Returns an easing run that finishes halfway through and returns.
     """
+    raise NotImplementedError
+    
     if isinstance(easing_func, str):
         easing_func = globals()[easing_func]
 
@@ -103,3 +116,4 @@ def easeReturn(easing_func, start, stop, frames):
     x1 = easing_func(stop, start, n1)()
 
     return np.hstack([x0, x1])
+
