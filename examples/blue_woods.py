2af01795db30d743c4bb6b68eb880053c1275aba
Travis Hoppe <travis.hoppe@gmail.com>
2018-12-13 19:25:10 -0500

    Some cleaner examples for blue_woods

--- examples/blue_woods.py
+++ examples/blue_woods.py
@@ -1,37 +1,56 @@
-from pixelhouse import Canvas, ellipse
+from pixelhouse import Canvas, circle
 import pixelhouse as ph
 import numpy as np
 
 
-def draw_circles(C, dx=0.50, tc=0.2):
+def draw_circles(C):
+    """
+    This function draws the shaded circles to the screen. We are drawing
+    them in a function so we can reuse them quickly for blending.
+    """
+
+    # Palette #19 is a good predefined palette with whites and blues
     pal = ph.palette(19)
 
-    base_r = 1.60
+    # The base radius of our circles. Everything will scale from this
+    radius = 1.60
+    offset = 0.50
+    tc = 0.2  # Base thickness
 
-    r = base_r + dx
+    # All the circles will have a linear gradient from top to bottom,
+    # or bottom to top (we use theta for that). The second parameter [1, 0]
+    # controls the alpha (the transparency). The sizing and thickness was
+    # choosen by hand to get a nice effect.
+
     lg = ph.gradient.linear([pal[0], pal[1]], [1, 0], theta=-np.pi / 2)
-    C += ellipse(0, 0, r, r, thickness=tc, gradient=lg)
+    C += circle(0, 0, radius + offset, thickness=tc, gradient=lg)
 
-    r = base_r - 2 * dx
     lg = ph.gradient.linear([pal[0], pal[1]], [1, 0], theta=np.pi / 2)
-    C += ellipse(0, 0, r, r, thickness=tc / 2, gradient=lg)
+    C += circle(0, 0, radius - 2 * offset, thickness=tc / 2, gradient=lg)
 
-    r = base_r - dx
     lg = ph.gradient.linear([pal[2], pal[1]], [1, 0], theta=-np.pi / 2)
-    C += ellipse(0, 0, r, r, thickness=tc / 2, gradient=lg)
+    C += circle(0, 0, radius - offset, thickness=tc / 2, gradient=lg)
 
-    r = base_r + 1.5 * dx
     lg = ph.gradient.linear([pal[2], pal[1]], [1, 0], theta=np.pi / 2)
-    C += ellipse(0, 0, r, r, thickness=tc / 2, gradient=lg)
+    C += circle(0, 0, radius + 1.5 * offset, thickness=tc / 2, gradient=lg)
 
 
+# Start a new canvas and load in an image. It will resize automatically.
 C = Canvas().load("asphalt-dark-dawn-531321.jpg")
+
+# Apply the instagram-like filter named 1977.
+# Use ph.filters.instafilter("") to get a list of all known.
 C += ph.filters.instafilter("1977")
 
-with C.layer() as C2:
-    draw_circles(C2)
-    C2 += ph.filters.gaussian_blur()
-    draw_circles(C2)
+# Start a new layer. This uses the current image as a background, and then
+# applies it after the context is over.
+with C.layer() as layer:
+
+    # Draw the circles, blend, then draw them again.
+    # This makes them have a glowy effect.
+    draw_circles(layer)
+    layer += ph.filters.gaussian_blur()
+    draw_circles(layer)
 
 C.save("figures/blue_woods.png")
 C.show()
