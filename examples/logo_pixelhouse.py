32ba8b17c2d9ebf6f9ad318b7ca948954c0459de
Travis Hoppe <travis.hoppe@gmail.com>
2018-12-13 20:24:48 -0500

    Add some comments to the logo example

--- examples/logo_pixelhouse.py
+++ examples/logo_pixelhouse.py
@@ -1,16 +1,23 @@
 import numpy as np
 import pixelhouse as ph
 
+# Palette #6 looks like the beach
 pal = ph.palette(6)
 
-C = ph.Canvas(400, 400, bg=pal[2])
+canvas = ph.Canvas(400, 400, bg=pal[2])
+
+# Draw a circle, fuzz it, then draw it again
+canvas += ph.circle(color=pal[3])
+canvas += ph.filters.gaussian_blur()
+canvas += ph.circle(color=pal[3])
+
+# Build a nice but subtle linear gradient for the text
+# Angle it at -45 degress (-pi/4)
 lg = ph.gradient.linear([pal[0], pal[1]], theta=-np.pi / 4)
 
-C += ph.circle(color=pal[3])
-C += ph.filters.gaussian_blur()
-C += ph.circle(color=pal[3])
+# Draw the text and repeat it vertically
 for i in np.arange(-6, 6, 1.0):
-    C += ph.text("pixelhouse", y=i, gradient=lg)
+    canvas += ph.text("pixelhouse", y=i, gradient=lg)
 
-C.save("figures/logo_pixelhouse.png")
-C.show()
+canvas.save("figures/logo_pixelhouse.png")
+canvas.show()
