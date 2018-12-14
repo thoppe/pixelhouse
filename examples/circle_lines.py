03e0c9f2a1ca4beed752e6eebca60025ab145d05
Travis Hoppe <travis.hoppe@gmail.com>
2018-12-13 19:03:54 -0500

    Make circle_links.py example more user friendly

--- examples/circle_lines.py
+++ examples/circle_lines.py
@@ -2,22 +2,41 @@ import random
 import numpy as np
 import pixelhouse as ph
 
+# Start with a good predefined color palette.
+# Palette 20 has a bunch of muted reds and blues.
 pal = ph.palette(20)
+
+# Split the colors into our primary and background colors.
+background_color, primary_colors = pal[0], pal[1:]
+
+# Since we are choosing the colors randomly, if we set a seed we get
+# the same result each time.
 random.seed(44)
 
-C = ph.Canvas(400, 400, bg=pal[0])
-r = 0.20
+# Setup our canvas! Let's make it 400 by 400 pixels, and use the first palette
+# color as the background.
+canvas = ph.Canvas(400, 400, bg=background_color)
 
-for k, y in enumerate(np.arange(-6, 6, r * 2)):
+# Draw circles in lines down the screen. To give it a wavy fell,
+# we want every other circle to be offset a bit.
+radius = 0.40
 
-    x0 = np.arange(-6, 6, r * 5)
-    if k % 2:
-        x0 += r
+for k, y in enumerate(np.arange(-6, 6, radius)):
 
-    colors = [random.choice(pal[1:]) for k in range(len(x0))]
+    for x in np.arange(-6, 6, radius * 2.5):
 
-    for k in range(len(x0)):
-        C += ph.circle(x=x0[k], y=y, r=2*r, color=colors[k])
+        # If k is odd (eg. every other one), offset it!
+        if k % 2:
+            x += radius/2
 
-C.save("figures/circle_lines.png")
-C.show()
+        # Pick a random color
+        color = random.choice(primary_colors)
+
+        # Draw a circle to the canvas by "adding" to it
+        canvas += ph.circle(x=x, y=y, r=radius, color=color)
+
+# Save the image.
+canvas.save("figures/circle_lines.png")
+
+# Display the image. It'll stay open until you press a key.
+canvas.show()
