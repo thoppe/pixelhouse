1fe02903d422419db4ed083e0f8d58d719ef066a
thoppe <travis.hoppe@gmail.com>
2019-05-02 14:29:12 -0400

    Fix images

--- unincorporated_demos/vapor.py
+++ unincorporated_demos/vapor.py
@@ -2,7 +2,7 @@ import pixelhouse as ph
 import numpy as np
 
 pal = ["#FF6AD5", "#C774E8", "#AD8CFF", "#8795E8", "#94D0FF"] * 3
-C = ph.Canvas(400, 400)
+C = ph.Canvas(200, 200)
 #C = ph.Animation(512, 512, fps=30, duration=4)
 
 lw = 0.25
@@ -14,7 +14,7 @@ for k, y in enumerate(Y):
     x = move(3, 0.25, flip=True, phase=k * dk)
     gx = move(1, 4, flip=True, phase=k * dk)
     line = ph.line(x=-x, x1=x, y=y, y1=y, thickness=lw, color=pal[k])
-    C += ph.filters.glow(line, glow_x=gx, glow_y=gx, n=5)
+    C += ph.filters.glow(line, glow_x=gx, glow_y=gx*0.5, n=15)
 
 
 #ph.canvas2mp4(C, "vwa.mp4")
